4072cc5ac3b3be791e857ae7c3435ef7de9e2025
andersres <andersresell10@gmail.com>
2022-10-28 11:32:00 +0200

    Update anders.py

--- anders.py
+++ anders.py
@@ -1,2 +1,16 @@
 #Anders
 
+from datetime import datetime
+
+class Avtale:
+    def __init__(self, tittel, sted, dato, varighet):
+        self.tittel=tittel      #str
+        self.sted=sted          #str
+        self.dato=dato          #datetime.datetime
+        self.varighet=varighet  #int
+        
+    def __str__(self): #
+        return f"{self.tittel}, {self.sted}, {self.dato}, {self.varighet}"
+        
+a1 = Avtale("m1", "her", "invalid", "invalid")
+print(a1)
